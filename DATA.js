2dd2e6b54b59e7dff68338dc43b0240898280196
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-16 14:23:26 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "2a121330-e19d-4b96-9dcf-8fe5ec84bfb0",
+    "home_feed": [
+        {
+            "id": "d518874f-040e-4975-9375-93ac01996c14",
+            "artist_name": "skin",
+            "home_carousel": [
+                {
+                    "id": "36a4f9f8-d01d-47d2-a902-8d98246ebf05",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                },
+                {
+                    "id": "6d96561a-d420-4224-93c9-2cf2174e8143",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                },
+                {
+                    "id": "9cf13a90-c3dc-4c43-9055-2c3dc95bdfd8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "02374e75-32fe-4fca-b8cd-7903c21e0d8d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                },
+                {
+                    "id": "79888b9f-8bf9-4374-9b42-7be949c78fd5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                }
+            ],
+            "art_caption": "about",
+            "art_description": "Support entire finally. Our be particular close fund. Ok another leader father respond management color treat. Wide summer result during shake."
+        },
+        {
+            "id": "141a588e-d5f7-4949-9657-eb48432b60f3",
+            "artist_name": "knowledge",
+            "home_carousel": [
+                {
+                    "id": "fb6ec4c2-3f2a-459a-9e0f-7609154a0d59",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/195.png"
+                },
+                {
+                    "id": "a2c63b6e-208e-4d0e-8f6d-e05fda7d2051",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/242.png"
+                },
+                {
+                    "id": "ac8dfdc3-6176-4312-a956-eb4fa35afde1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                },
+                {
+                    "id": "85527058-464a-46ab-a979-84ec1ee1ac13",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/42.png"
+                },
+                {
+                    "id": "17331724-77ab-4acf-876a-3f02944f9f5e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                }
+            ],
+            "art_caption": "important",
+            "art_description": "Page century action drive support player letter. New history simply suddenly degree result mean organization. Radio cold ball receive product present again. Start price their benefit beat box."
+        },
+        {
+            "id": "4629e971-3501-4bea-8069-b759bc6bf04d",
+            "artist_name": "buy",
+            "home_carousel": [
+                {
+                    "id": "2e213dd0-4c17-4b11-aac1-c0b8a6c804df",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                },
+                {
+                    "id": "e1445fb7-3f5b-445d-8d04-2ac0dc6cd41b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                },
+                {
+                    "id": "00123593-99f5-4f87-a8eb-96efbd4f5f9a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png"
+                },
+                {
+                    "id": "149b09c8-fff6-48de-ab0f-dd93bc7d321f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "1828f23b-5575-4a33-8028-a678c5248813",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                }
+            ],
+            "art_caption": "answer",
+            "art_description": "Line style strategy political move collection page power. Hard moment never population."
+        },
+        {
+            "id": "b5cb1cd9-1940-43d2-a5e0-1f3b6b93d0ae",
+            "artist_name": "special",
+            "home_carousel": [
+                {
+                    "id": "a9bc6a99-ab99-4453-90c0-b75f693b8165",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                },
+                {
+                    "id": "c1be513c-8cb1-4af3-8285-c3c3c98f2f87",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/129.png"
+                },
+                {
+                    "id": "8380bcee-0d2c-4771-a288-38acc719d1f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "2e425663-2261-456f-ac2e-d5bd673f2687",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/162.png"
+                },
+                {
+                    "id": "4d9d3e5c-7bad-49a1-b112-91bdb04f4c0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                }
+            ],
+            "art_caption": "modern",
+            "art_description": "Community medical bad step Mrs listen. Help bed win fall high executive."
+        },
+        {
+            "id": "46086c5f-1891-4c08-82df-bde3fa50803b",
+            "artist_name": "director",
+            "home_carousel": [
+                {
+                    "id": "22ea5881-ba99-473b-854d-6ce79b6abaef",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                },
+                {
+                    "id": "cdbe6347-88c7-429f-8adf-75c6aa7d0758",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                },
+                {
+                    "id": "9ccc5189-8112-4e64-bccb-7a019e409e1c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/129.png"
+                },
+                {
+                    "id": "1d9ee3ce-ec73-4c73-9ea6-9e17a3400d8b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/196.png"
+                },
+                {
+                    "id": "8276d61b-b0c2-47ff-89b2-caaa3ed0aab5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png"
+                }
+            ],
+            "art_caption": "degree",
+            "art_description": "Memory share relate lead."
+        }
+    ]
+}
